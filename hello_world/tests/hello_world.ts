ef0a05fe477f3adf89e7f0d0982280c237cbc0b2
ALsJourney <aldin.dino@outlook.com>
2023-05-12 23:30:11 +0200

    Doing the chapter 1 hello world á la solana

---
+++ hello_world/tests/hello_world.ts
@@ -0,0 +1,66 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { HelloWorld } from "../target/types/hello_world";
+import chai from "chai";
+import { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised);
+
+describe("hello_world", () => {
+    // Configure the client to use the local cluster.
+    anchor.setProvider(anchor.AnchorProvider.env());
+
+    const program = anchor.workspace.HelloWorld as Program<HelloWorld>;
+
+    it("Is initialized!", async () => {
+        // Add your test here.
+        const tx = await program.methods.initialize().rpc();
+        console.log("Your transaction signature", tx);
+    });
+
+    const createWallet = async (
+        connection: anchor.web3.Connection,
+        funds: number
+    ): Promise<anchor.web3.Keypair> => {
+        const wallet = anchor.web3.Keypair.generate();
+        const tx = await connection.requestAirdrop(
+            wallet.publicKey,
+            anchor.web3.LAMPORTS_PER_SOL * funds
+        );
+
+        // wait for confirmation
+        const latestBlockHash = await connection.getLatestBlockhash();
+
+        await connection.confirmTransaction({
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            signature: tx,
+        });
+        const balance = await connection.getBalance(wallet.publicKey);
+        if (balance < funds) {
+            throw new Error(
+                "Requested amount exceeds target network's airdrop limit"
+            );
+        }
+        return wallet;
+    };
+
+    const initializeAccount = async (
+        program: Program<HelloWorld>,
+        authority: anchor.web3.Keypair
+    ): Promise<anchor.web3.PublicKey> => {
+        const accountKeypair = anchor.web3.Keypair.generate();
+
+        await program.methods
+            .initialize()
+            .accounts({
+                myAccount: accountKeypair.publicKey,
+                authority: authority.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            })
+            .signers([authority, accountKeypair])
+            .rpc();
+
+        return accountKeypair.publicKey;
+    };
+});
